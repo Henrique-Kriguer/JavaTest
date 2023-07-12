500c928a39976b8450bcf80fcf5be63df480449d
henrique-kriguer <henriquekriguer@gmail.com>
2023-07-12 09:50:00 -0300

    modificando o método de teste para apresnetar os erros separadamente, sem parar no primeiro erro do código, usando @Rule e errorCollector

---
+++ 3/TestesUnitarios+-+inicio/TestesUnitarios/src/test/java/br/ce/wcaquino/servicos/LocacaoServiceTest3.java
@@ -0,0 +1,38 @@
+package br.ce.wcaquino.servicos;
+
+import br.ce.wcaquino.entidades.Filme;
+import br.ce.wcaquino.entidades.Locacao;
+import br.ce.wcaquino.entidades.Usuario;
+import br.ce.wcaquino.utils.DataUtils;
+import org.hamcrest.CoreMatchers;
+import org.junit.Assert;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ErrorCollector;
+
+import java.util.Date;
+
+public class LocacaoServiceTest3 {
+
+    @Rule
+    public ErrorCollector error = new ErrorCollector();
+    @Test
+    public void test() {
+        //cenario
+        locacaoService service = new locacaoService();
+        Usuario usuario = new Usuario("Usuário 1");
+        Filme filme = new Filme("filmeLocado1",2,4.5);
+
+
+        //ação
+        Locacao locacao = service.alugarFilme(usuario, filme);
+
+        //Verificação
+       error.checkThat(locacao.getValor(), CoreMatchers.is( 4.0));
+       error.checkThat(DataUtils.isMesmaData(locacao.getDataLocacao(),new Date()),CoreMatchers.is(true));
+       error.checkThat(DataUtils.isMesmaData(locacao.getDataRetorno(), DataUtils.obterDataComDiferencaDias(1)),CoreMatchers.is(false));
+
+
+    }
+
+}
