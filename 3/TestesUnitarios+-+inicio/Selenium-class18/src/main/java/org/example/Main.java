2a220fe060c53566f45a1f46380989550eaec6ad
henrique-kriguer <henriquekriguer@gmail.com>
2023-07-18 16:17:26 -0300

    aula 21- atualizando selenium-java p/ 4.9.1 e rodando testes funcionais com selenium usando Chrome e Edge sem erros

--- 3/TestesUnitarios+-+inicio/Selenium-class18/src/main/java/org/example/Main.java
+++ 3/TestesUnitarios+-+inicio/Selenium-class18/src/main/java/org/example/Main.java
@@ -2,13 +2,18 @@ package org.example;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class Main {
-    @Test
+/*    @Test
     public void testeFirefox() {
         System.setProperty("web-driver.gecko.driver","C:/Development/JAVA/ProgramsDownloaded/geckodriver-r593/geckodriver.exe");
         WebDriver driver = new FirefoxDriver();
@@ -16,19 +21,28 @@ public class Main {
         System.out.println(driver.getTitle());
         Assertions.assertEquals("Google",driver.getTitle());
     }
-    @Test
-    public void testeChrome(){
+  */
+/*@Test
+    public void testeChrome() {
         System.setProperty("web-driver.chromedriver","C:/Development/JAVA/ProgramsDownloaded/cromedriver114/chromedriver.exe");
-        WebDriver driver = new ChromeDriver();
-        driver.get("https://www.google.com");
-        Assertions.assertEquals("Google",driver.getTitle());
-    }
-    @Test
-    public void testeEdge(){
-        System.setProperty("web-driver.msedgedriver","C:/Development/JAVA/ProgramsDownloaded/edgedriver117/msedgedriver.exe");
-        WebDriver driver = new EdgeDriver();
-        driver.get("https://www.google.com");
-        Assertions.assertEquals("Google",driver.getTitle());
-    }
+       WebDriver driver = new ChromeDriver();
+      driver.get("https://www.google.com");
+     Assertions.assertEquals("Google",driver.getTitle());
+    System.out.println(driver.getTitle());
+    driver.quit();
+
+
+}
+*/
+
+   @Test
+   public void testeEdge(){
+       System.setProperty("web-driver.msedgedriver","C:/Development/JAVA/ProgramsDownloaded/edgedriver117/msedgedriver.exe");
+      WebDriver driver = new EdgeDriver();
+      driver.get("https://www.google.com");
+      Assertions.assertEquals("Google",driver.getTitle());
+       System.out.println(driver.getTitle());
+      driver.quit();
+   }
 
 }
