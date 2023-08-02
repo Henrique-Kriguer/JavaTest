abe6a29c0476e2d8775a53ab3ec4678559fb0c7a
Henrique Kriguer <hkriguer@intra.fazenda.sp.gov.br>
2023-08-02 11:36:10 -0300

    Automação em elementos radio buton e check box

--- 3/TestesUnitarios+-+inicio/Selenium-class18/src/main/java/org/example/TesteCampoTreinamento.java
+++ 3/TestesUnitarios+-+inicio/Selenium-class18/src/main/java/org/example/TesteCampoTreinamento.java
@@ -8,16 +8,43 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class TesteCampoTreinamento {
-
-    public void testeTextField(){
-        System.setProperty("web-driver.gecko.driver","C:/Learning/Selenium-Java/geckodriver-v0.33.0-win-aarch64/geckodriver.exe");
+    @Test
+    public void testeTextField() {
+        System.setProperty("web-driver.gecko.driver", "C:/Learning/Selenium-Java/geckodriver-v0.33.0-win-aarch64/geckodriver.exe");
         WebDriver driver = new FirefoxDriver();
         driver.get("file:///" + System.getProperty("user.dir") + "/src/main/resources/componentes.html");
         driver.findElement(By.id("elementosForm:nome")).sendKeys("Teste de escrita");
         Assertions.assertEquals("Teste de escrita", driver.findElement(By.id("elementosForm:nome")).getAttribute("value"));
-        driver.quit();
+       // driver.quit();
     }
-*/
+        @Test
+        public void testeTextArea(){
+            System.setProperty("web-driver.gecko.driver","C:/Learning/Selenium-Java/geckodriver-v0.33.0-win-aarch64/geckodriver.exe");
+            WebDriver driver = new FirefoxDriver();
+            driver.get("file:///" + System.getProperty("user.dir") + "/src/main/resources/componentes.html");
+            driver.findElement(By.id("elementosForm:sugestoes")).sendKeys("Teste de escrita 2");
+            Assertions.assertEquals("Teste de escrita 2", driver.findElement(By.id("elementosForm:sugestoes")).getAttribute("value"));
+            //driver.quit();
+    }
+
+    @Test
+    public void testeRadioButtonn() {
+        System.setProperty("web-driver.gecko.driver", "C:/Learning/Selenium-Java/geckodriver-v0.33.0-win-aarch64/geckodriver.exe");
+        WebDriver driver = new FirefoxDriver();
+        driver.get("file:///" + System.getProperty("user.dir") + "/src/main/resources/componentes.html");
+        driver.findElement(By.id("elementosForm:sexo:0")).click();
+        Assertions.assertTrue(driver.findElement(By.id("elementosForm:sexo:0")).isSelected());
+    }
+
+    @Test
+    public void testeCheckBox() {
+        System.setProperty("web-driver.gecko.driver", "C:/Learning/Selenium-Java/geckodriver-v0.33.0-win-aarch64/geckodriver.exe");
+        WebDriver driver = new FirefoxDriver();
+        driver.get("file:///" + System.getProperty("user.dir") + "/src/main/resources/componentes.html");
+        driver.findElement(By.id("elementosForm:comidaFavorita:2")).click();
+        Assertions.assertTrue(driver.findElement(By.id("elementosForm:comidaFavorita:2")).isSelected());
+    }
+/*
     @Test
     public void testeInteragindoTextArea(){
         WebDriver driver = new FirefoxDriver();
@@ -33,6 +60,6 @@ public class TesteCampoTreinamento {
         driver.get("file:///" + System.getProperty("user.dir") + "/src/main/resources/componentes.html");
 
     }
+*/
 
- */
 }
