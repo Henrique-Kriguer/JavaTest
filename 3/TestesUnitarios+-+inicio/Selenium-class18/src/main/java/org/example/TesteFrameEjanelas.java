8c462555ddc25385821b02996a3dfc9a9503dd6f
Henrique Kriguer <henriquekriguer@gmail.com>
2023-09-15 12:40:24 -0300

    Class38-windowHandler

--- 3/TestesUnitarios+-+inicio/Selenium-class18/src/main/java/org/example/TesteFrameEjanelas.java
+++ 3/TestesUnitarios+-+inicio/Selenium-class18/src/main/java/org/example/TesteFrameEjanelas.java
@@ -36,7 +36,23 @@ public class TesteFrameEjanelas {
         driver.switchTo().window("Popup");
         driver.findElement(By.tagName("textarea")).sendKeys("Deu certo !!");
         driver.close();
-      
+    }
+
+    @Test
+    public void testeScriptJanelaSemTitulo() {
+        System.setProperty("web-driver.gecko.driver", "C:/Learning/Selenium-Java/geckodriver-v0.33.0-win-aarch64/geckodriver.exe");
+        WebDriver driver = new FirefoxDriver();
+        driver.manage().window().setSize(new Dimension(1920, 1080));
+        driver.get("file:///" + System.getProperty("user.dir") + "/src/main/resources/componentes.html");
+        driver.findElement(By.id("ButtonPopUpHard")).click();
+        System.out.println(driver.getWindowHandle());
+        System.out.println(driver.getWindowHandles());
+        driver.switchTo().window((String) driver.getWindowHandles().toArray()[1]);
+        driver.findElement(By.tagName("textarea")).sendKeys("Escrevi aqui !");
+        driver.switchTo().window((String) driver.getWindowHandles().toArray()[0]);
+        driver.findElement(By.tagName("textarea")).sendKeys("Escrevi aqui também !");
+
+
 
     }
 }
