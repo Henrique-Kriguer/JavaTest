7a502e997087785a9d8eb12809100ba0364e500d
Henrique Kriguer <henriquekriguer@gmail.com>
2023-09-25 17:08:32 -0300

    Refatorei a classe FramesEJanelas implantando annotations BeforeAll e AftherAll para permitir reuso de código.

--- 3/TestesUnitarios+-+inicio/Selenium-class18/src/main/java/org/example/TesteFrameEjanelas.java
+++ 3/TestesUnitarios+-+inicio/Selenium-class18/src/main/java/org/example/TesteFrameEjanelas.java
@@ -1,6 +1,8 @@
 package org.example;
 
+import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
@@ -10,12 +12,23 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class TesteFrameEjanelas {
 
-    @Test
-    public void testeBotaoDentroFrame() {
+    private static WebDriver driver;
+
+    @BeforeAll
+    public static void inicializa(){
         System.setProperty("web-driver.gecko.driver", "C:/Learning/Selenium-Java/geckodriver-v0.33.0-win-aarch64/geckodriver.exe");
-        WebDriver driver = new FirefoxDriver();
+        driver = new FirefoxDriver();
         driver.manage().window().setSize(new Dimension(1920, 1080));
         driver.get("file:///" + System.getProperty("user.dir") + "/src/main/resources/componentes.html");
+    }
+
+    @AfterAll
+    public static void finaliza(){
+        driver.quit();
+    }
+    @Test
+    public void testeBotaoDentroFrame() {
+
         driver.switchTo().frame("frame1");
         driver.findElement(By.id("frameButton")).click();
         Alert alert = driver.switchTo().alert();
@@ -28,10 +41,6 @@ public class TesteFrameEjanelas {
 
     @Test
     public void testeScriptJanela() {
-        System.setProperty("web-driver.gecko.driver", "C:/Learning/Selenium-Java/geckodriver-v0.33.0-win-aarch64/geckodriver.exe");
-        WebDriver driver = new FirefoxDriver();
-        driver.manage().window().setSize(new Dimension(1920, 1080));
-        driver.get("file:///" + System.getProperty("user.dir") + "/src/main/resources/componentes.html");
         driver.findElement(By.id("buttonPopUpEasy")).click();
         driver.switchTo().window("Popup");
         driver.findElement(By.tagName("textarea")).sendKeys("Deu certo !!");
@@ -40,10 +49,6 @@ public class TesteFrameEjanelas {
 
     @Test
     public void testeScriptJanelaSemTitulo() {
-        System.setProperty("web-driver.gecko.driver", "C:/Learning/Selenium-Java/geckodriver-v0.33.0-win-aarch64/geckodriver.exe");
-        WebDriver driver = new FirefoxDriver();
-        driver.manage().window().setSize(new Dimension(1920, 1080));
-        driver.get("file:///" + System.getProperty("user.dir") + "/src/main/resources/componentes.html");
         driver.findElement(By.id("ButtonPopUpHard")).click();
         System.out.println(driver.getWindowHandle());
         System.out.println(driver.getWindowHandles());
@@ -52,7 +57,5 @@ public class TesteFrameEjanelas {
         driver.switchTo().window((String) driver.getWindowHandles().toArray()[0]);
         driver.findElement(By.tagName("textarea")).sendKeys("Escrevi aqui também !");
 */
-
-
     }
 }
