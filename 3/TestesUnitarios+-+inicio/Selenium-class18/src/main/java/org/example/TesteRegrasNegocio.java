5e66abb61e77269fb9ebd69ae7ebf49f954b7499
Henrique Kriguer <henriquekriguer@gmail.com>
2023-09-25 17:29:05 -0300

    Refatorei a classe teste regras de negocio implantando annotations BeforeAll e AftherAll para permitir reuso de código.

--- 3/TestesUnitarios+-+inicio/Selenium-class18/src/main/java/org/example/TesteRegrasNegocio.java
+++ 3/TestesUnitarios+-+inicio/Selenium-class18/src/main/java/org/example/TesteRegrasNegocio.java
@@ -1,6 +1,8 @@
 package org.example;
 
+import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
@@ -11,79 +13,80 @@ import org.openqa.selenium.support.ui.Select;
 
 public class TesteRegrasNegocio {
 
-        @Test
-        public void testeNameTextField() {
-            System.setProperty("web-driver.gecko.driver", "C:/Learning/Selenium-Java/geckodriver-v0.33.0-win-aarch64/geckodriver.exe");
-            WebDriver driver = new FirefoxDriver();
-            driver.get("file:///" + System.getProperty("user.dir") + "/src/main/resources/componentes.html");
-            driver.findElement(By.id("elementosForm:cadastrar")).click();
-            Alert alert = driver.switchTo().alert();
-            Assertions.assertEquals("Nome eh obrigatorio", alert.getText());
-            alert.accept();
-            driver.findElement(By.id("elementosForm:nome")).sendKeys("Henrique");
-            driver.findElement(By.id("elementosForm:cadastrar")).click();
-            alert.accept();
-            driver.quit();
-        }
+    private static WebDriver driver;
 
-        @Test
-        public void testeSurNameTextfield() {
-            System.setProperty("web-driver.gecko.driver", "C:/Learning/Selenium-Java/geckodriver-v0.33.0-win-aarch64/geckodriver.exe");
-            WebDriver driver = new FirefoxDriver();
-            driver.get("file:///" + System.getProperty("user.dir") + "/src/main/resources/componentes.html");
-            driver.findElement(By.id("elementosForm:nome")).sendKeys("Henrique");
-            driver.findElement(By.id("elementosForm:cadastrar")).click();
-            Alert alert = driver.switchTo().alert();
-            Assertions.assertEquals("Sobrenome eh obrigatorio", alert.getText());
-            alert.accept();
-            driver.findElement(By.id("elementosForm:sobrenome")).sendKeys("Kriguer");
-            driver.findElement(By.id("elementosForm:cadastrar")).click();
-            alert.accept();
-            driver.quit();
-        }
+    @BeforeAll
+    public static void inicializa() {
+        System.setProperty("web-driver.gecko.driver", "C:/Learning/Selenium-Java/geckodriver-v0.33.0-win-aarch64/geckodriver.exe");
+        driver = new FirefoxDriver();
+        driver.get("file:///" + System.getProperty("user.dir") + "/src/main/resources/componentes.html");
+    }
 
-            @Test
-            public void testeSexoRadioButton(){
-            System.setProperty("web-driver.gecko.driver", "C:/Learning/Selenium-Java/geckodriver-v0.33.0-win-aarch64/geckodriver.exe");
-            WebDriver driver = new FirefoxDriver();
-            driver.get("file:///" + System.getProperty("user.dir") + "/src/main/resources/componentes.html");
-            driver.findElement(By.id("elementosForm:nome")).sendKeys("Henrique");
-            driver.findElement(By.id("elementosForm:sobrenome")).sendKeys("Kriguer");
-            driver.findElement(By.id("elementosForm:cadastrar")).click();
-            Alert alert = driver.switchTo().alert();
-            Assertions.assertEquals("Sexo eh obrigatorio", alert.getText());
-            alert.accept();
-            driver.findElement(By.id("elementosForm:sexo:0")).click();
-            driver.findElement(By.id("elementosForm:cadastrar")).click();
-            driver.quit();
-        }
+    @AfterAll
+    public static void finaliza() {
+        driver.quit();
+    }
 
-             @Test
-                 public void testeCheckBox() {
-                     System.setProperty("web-driver.gecko.driver", "C:/Learning/Selenium-Java/geckodriver-v0.33.0-win-aarch64/geckodriver.exe");
-                     WebDriver driver = new FirefoxDriver();
-                     driver.get("file:///" + System.getProperty("user.dir") + "/src/main/resources/componentes.html");
-                     driver.findElement(By.id("elementosForm:nome")).sendKeys("Henrique");
-                     driver.findElement(By.id("elementosForm:sobrenome")).sendKeys("Kriguer");
-                     driver.findElement(By.id("elementosForm:sexo:0")).click();
-                     driver.findElement(By.id("elementosForm:sexo:1")).click();
-                     driver.findElement(By.id("elementosForm:comidaFavorita:0")).click();
-                     driver.findElement(By.id("elementosForm:comidaFavorita:1")).click();
-                     driver.findElement(By.id("elementosForm:comidaFavorita:2")).click();
-                     driver.findElement(By.id("elementosForm:comidaFavorita:3")).click();
-                     driver.findElement(By.id("elementosForm:cadastrar")).click();
-                     Alert alert = driver.switchTo().alert();
-                     Assertions.assertEquals("Tem certeza que voce eh vegetariano?", alert.getText());
-                     alert.accept();
-                     driver.findElement(By.id("elementosForm:comidaFavorita:3")).click();
-                     driver.findElement(By.id("elementosForm:cadastrar")).click();
+    @Test
+    public void testeNameTextField() {
 
-             }
+        driver.findElement(By.id("elementosForm:cadastrar")).click();
+        Alert alert = driver.switchTo().alert();
+        Assertions.assertEquals("Nome eh obrigatorio", alert.getText());
+        alert.accept();
+        driver.findElement(By.id("elementosForm:nome")).sendKeys("Henrique");
+        driver.findElement(By.id("elementosForm:cadastrar")).click();
+        alert.accept();
+       // driver.quit();
+    }
+
+    @Test
+    public void testeSurNameTextfield() {
+        driver.findElement(By.id("elementosForm:nome")).sendKeys("Henrique");
+        driver.findElement(By.id("elementosForm:cadastrar")).click();
+        Alert alert = driver.switchTo().alert();
+        Assertions.assertEquals("Sobrenome eh obrigatorio", alert.getText());
+        alert.accept();
+        driver.findElement(By.id("elementosForm:sobrenome")).sendKeys("Kriguer");
+        driver.findElement(By.id("elementosForm:cadastrar")).click();
+        alert.accept();
+        //driver.quit();
+    }
+
+    @Test
+    public void testeSexoRadioButton() {
+        driver.findElement(By.id("elementosForm:nome")).sendKeys("Henrique");
+        driver.findElement(By.id("elementosForm:sobrenome")).sendKeys("Kriguer");
+        driver.findElement(By.id("elementosForm:cadastrar")).click();
+        Alert alert = driver.switchTo().alert();
+        Assertions.assertEquals("Sexo eh obrigatorio", alert.getText());
+        alert.accept();
+        driver.findElement(By.id("elementosForm:sexo:0")).click();
+        driver.findElement(By.id("elementosForm:cadastrar")).click();
+        //driver.quit();
+    }
+
+    @Test
+    public void testeCheckBox() {
+        driver.findElement(By.id("elementosForm:nome")).sendKeys("Henrique");
+        driver.findElement(By.id("elementosForm:sobrenome")).sendKeys("Kriguer");
+        driver.findElement(By.id("elementosForm:sexo:0")).click();
+        driver.findElement(By.id("elementosForm:sexo:1")).click();
+        driver.findElement(By.id("elementosForm:comidaFavorita:0")).click();
+        driver.findElement(By.id("elementosForm:comidaFavorita:1")).click();
+        driver.findElement(By.id("elementosForm:comidaFavorita:2")).click();
+        driver.findElement(By.id("elementosForm:comidaFavorita:3")).click();
+        driver.findElement(By.id("elementosForm:cadastrar")).click();
+        Alert alert = driver.switchTo().alert();
+        Assertions.assertEquals("Tem certeza que voce eh vegetariano?", alert.getText());
+        alert.accept();
+        driver.findElement(By.id("elementosForm:comidaFavorita:3")).click();
+        driver.findElement(By.id("elementosForm:cadastrar")).click();
+       // driver.quit();
+    }
+
     @Test
     public void testeCombo() {
-        System.setProperty("web-driver.gecko.driver", "C:/Learning/Selenium-Java/geckodriver-v0.33.0-win-aarch64/geckodriver.exe");
-        WebDriver driver = new FirefoxDriver();
-        driver.get("file:///" + System.getProperty("user.dir") + "/src/main/resources/componentes.html");
         driver.findElement(By.id("elementosForm:nome")).sendKeys("Henrique");
         driver.findElement(By.id("elementosForm:sobrenome")).sendKeys("Kriguer");
         driver.findElement(By.id("elementosForm:sexo:0")).click();
@@ -100,5 +103,6 @@ public class TesteRegrasNegocio {
         alert.accept();
         combo.deselectByVisibleText("O que eh esporte?");
         driver.findElement(By.id("elementosForm:cadastrar")).click();
+        //driver.quit();
     }
 }
