b9e62c8ce4d5aa04d172eafdc6fd1545f8173f9c
Henrique Kriguer <henriquekriguer@gmail.com>
2023-11-07 15:15:04 -0300

    class-42 resuso de metodos- DSL

--- 3/TestesUnitarios+-+inicio/Selenium-class18/src/main/java/org/example/DSL.java
+++ 3/TestesUnitarios+-+inicio/Selenium-class18/src/main/java/org/example/DSL.java
@@ -1,7 +1,10 @@
 package org.example;
 
+import org.junit.jupiter.api.Assertions;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.Select;
 
 public class DSL {
 
@@ -13,5 +16,47 @@ public class DSL {
 
     public void escreve(String idCampo, String texto){
         driver.findElement(By.id(idCampo)).sendKeys(texto);
+    }
+
+    public String obterValor(String idCampo){
+        return driver.findElement(By.id(idCampo)).getAttribute("value");
+    }
+
+    public void clicarRadio(String id){
+        driver.findElement(By.id(id)).click();
+    }
+
+    public boolean isRadioSelected(String id){
+       return driver.findElement(By.id(id)).isSelected();
+       }
+
+    public void selecionaCombo(String id, String valor){
+        WebElement element = driver.findElement(By.id(id));
+        Select combo = new Select(element);
+        combo.selectByVisibleText(valor);
+    }
+
+    public String obterValorCombo(String id){
+        WebElement element = driver.findElement(By.id(id));
+        Select combo = new Select(element);
+        return combo.getFirstSelectedOption().getText();
+    }
+
+    public void clicarBtn(String id){
+        driver.findElement(By.id(id)).click();
+    }
+
+    public void clicarLink(String id){
+       driver.findElement(By.linkText(id)).click();
+    }
+
+    public String obterTexto(String id){
+        return driver.findElement(By.id(id)).getText();
+    }
+    public String obterTextoTag(String id){
+        return driver.findElement(By.tagName(id)).getText();
+    }
+    public String obterTextoClass(String id){
+        return driver.findElement(By.className(id)).getText();
     }
 }
--- 3/TestesUnitarios+-+inicio/Selenium-class18/src/main/java/org/example/TesteCampoTreinamento.java
+++ 3/TestesUnitarios+-+inicio/Selenium-class18/src/main/java/org/example/TesteCampoTreinamento.java
@@ -19,11 +19,14 @@ import java.util.List;
 public class TesteCampoTreinamento {
 
     private static WebDriver driver = new EdgeDriver();
+    private static DSL dsl;
+
     @BeforeAll
     public static void inicializa(){
+        dsl = new DSL(driver);
         System.setProperty("web-driver.msedgedriver","C:/Learning/Selenium-Java/edgedriver_win64/msedgedriver.exe");
         driver.get("file:///" + System.getProperty("user.dir") + "/src/main/resources/componentes.html");
-       // dsl = new DSL(driver);
+
     }
     @AfterAll
     public static void finaliza(){
@@ -31,44 +34,41 @@ public class TesteCampoTreinamento {
     }
       @Test
     public void testeTextField() {
-
-        driver.findElement(By.id("elementosForm:nome")).sendKeys("Teste de escrita");
-        Assertions.assertEquals("Teste de escrita", driver.findElement(By.id("elementosForm:nome")).getAttribute("value"));
+        dsl.escreve("elementosForm:nome", "Teste de escrita");
+        Assertions.assertEquals("Teste de escrita", dsl.obterValor("elementosForm:nome"));
     }
 
     @Test
     public void testeTextArea() {
-       driver.findElement(By.id("elementosForm:sugestoes")).sendKeys("Teste de escrita 2");
+        dsl.escreve("elementosForm:sugestoes","Teste de escrita 2");
        Assertions.assertEquals("Teste de escrita 2", driver.findElement(By.id("elementosForm:sugestoes")).getAttribute("value"));
     }
 
     @Test
     public void testeRadioButtonn() {
-        driver.findElement(By.id("elementosForm:sexo:1")).click();
-        Assertions.assertTrue(driver.findElement(By.id("elementosForm:sexo:1")).isSelected());
+        dsl.clicarRadio("elementosForm:sexo:1");
+        Assertions.assertTrue(dsl.isRadioSelected("elementosForm:sexo:1"));
     }
 
     @Test
     public void testeCheckBox() {
-        driver.findElement(By.id("elementosForm:comidaFavorita:0")).click();
-        Assertions.assertTrue(driver.findElement(By.id("elementosForm:comidaFavorita:0")).isSelected());
+        dsl.clicarRadio("elementosForm:comidaFavorita:0");
+        Assertions.assertTrue(dsl.isRadioSelected("elementosForm:comidaFavorita:0"));
     }
 
     @Test
     public void testeComboBox() {
-        WebElement element = driver.findElement(By.id("elementosForm:escolaridade"));
-        Select combo = new Select(element);
-        combo.selectByVisibleText("Superior");
-        Assertions.assertEquals("Superior", combo.getFirstSelectedOption().getText());
+        dsl.selecionaCombo("elementosForm:escolaridade","Superior");
+        Assertions.assertEquals("Superior", dsl.obterValorCombo("elementosForm:escolaridade"));
     }
 
     @Test
     public void testeComboBoxMultiplo() {
+        dsl.selecionaCombo("elementosForm:esportes","Natacao");
+        dsl.selecionaCombo("elementosForm:esportes","Corrida");
+        dsl.selecionaCombo("elementosForm:esportes","Futebol");
         WebElement element = driver.findElement(By.id("elementosForm:esportes"));
         Select combo = new Select(element);
-        combo.selectByVisibleText("Natacao");
-        combo.selectByVisibleText("Corrida");
-        combo.selectByVisibleText("Futebol");
         List<WebElement> allSellectedOptions = combo.getAllSelectedOptions();
         Assertions.assertEquals(3, allSellectedOptions.size());
         combo.deselectAll(); // esta opção desmarcados itens marcados anteriormente.
@@ -76,27 +76,27 @@ public class TesteCampoTreinamento {
 
     @Test
     public void testeBtnObrigado() {
-        driver.get("file:///" + System.getProperty("user.dir") + "/src/main/resources/componentes.html");
+
         WebElement botao = driver.findElement(By.id("buttonSimple"));
-        botao.click();
-        Assertions.assertEquals("Obrigado!", botao.getAttribute("value"));
+        Assertions.assertEquals("Clique Me!",botao.getAttribute("value"));
+        dsl.clicarBtn("buttonSimple");
+        botao = driver.findElement(By.id("buttonSimple"));
+        Assertions.assertEquals("Obrigado!",botao.getAttribute("value"));
     }
 
     @Test
     //@Ignore // esta anotação não funciona, permite pular o teste em uma bateria de testes, mostrando a msg skipped
     public void testeLink() {
-        driver.get("file:///" + System.getProperty("user.dir") + "/src/main/resources/componentes.html");
-        WebElement link = driver.findElement(By.linkText("Voltar"));
-        link.click();
-        Assertions.assertEquals("Voltou!", driver.findElement(By.id("resultado")).getText());
+        dsl.clicarLink("Voltar");
+        Assertions.assertEquals("Voltou!", dsl.obterTexto("resultado"));
     }
     @Test
     public void testeTextoPagina() {
         //Assertions.assertTrue(driver.findElement(By.tagName("body")).getText().contains("Campo de Treinamento"));
         // Este metodo acima é ineficaz pois o java tem que varrer todo body para encontrar o elemento.
         // Caso a tag h3 tivesse mais de uma ocorrencia, o teste daria erro pois ele vai no primeiro tag que encontrar.
-        Assertions.assertTrue(driver.findElement(By.tagName("h3")).getText().contains("Campo de Treinamento"));
-        Assertions.assertTrue(driver.findElement(By.className("facilAchar")).getText().contains("Cuidado onde clica, muitas armadilhas..."));
+        Assertions.assertTrue(dsl.obterTextoTag("h3").contains("Campo de Treinamento"));
+        Assertions.assertTrue(dsl.obterTextoClass("facilAchar").contains("Cuidado onde clica, muitas armadilhas..."));
     }
 }
 
